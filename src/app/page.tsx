5ee061a7e00a746d0725d1e7360d8a8e453ca245
notthefaa <a.gornakov@gmail.com>
2026-03-26 21:08:41 +0000

    UI Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -326,7 +326,7 @@ export default function FleetTrackerApp() {
   return (
     <>
       <style dangerouslySetInnerHTML={{__html: `html, body { background-color: #ffffff !important; }` }} />
-      <div className="flex flex-col bg-neutral-100 fixed inset-0 overflow-hidden">
+      <div className="flex flex-col bg-neutral-100 w-full overflow-hidden relative" style={{ height: '100vh' }}>
 
       <TutorialModal session={session} role={role} />
 
