bef1a6b8096e43c2449ae907c14f727fcfce33de
notthefaa <a.gornakov@gmail.com>
2026-03-26 20:48:26 +0000

    UI Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -470,7 +470,7 @@ export default function FleetTrackerApp() {
             <button 
               key={tab.id} 
               onClick={() => setActiveTab(tab.id as AppTab)} 
-              className={`flex-1 py-4 flex flex-col items-center justify-center transition-all relative active:scale-95 ${getTabColor(tab.id)}`}
+              className={`flex-1 py-3 flex flex-col items-center justify-center transition-all relative active:scale-95 ${getTabColor(tab.id)}`}
             >
               <div className="relative mb-1">
                 <tab.icon size={20} />
